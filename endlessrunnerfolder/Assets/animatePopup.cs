6c19186a7b573da09690d2695b7c2ac6e3fb0200
BlairCorban <blaircorban@hotmail.com>
2018-06-06 11:21:07 +1200

    revised popup system and couple point system things

--- endlessrunnerfolder/Assets/animatePopup.cs
+++ endlessrunnerfolder/Assets/animatePopup.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 public class animatePopup : MonoBehaviour {
-
+    public float movespeed;
+    public CanvasGroup uiElement;
+    public Vector3 startpos;
+    
 	// Use this for initialization
-	void OnEnable () {
-        iTween.ShakePosition(gameObject, iTween.Hash("x", 10.3f, "y", 10.3f, "time", 1.5f));
-        Invoke("destroyself", 1.5f);
-        
+    void Start()
+    {
+        StartCoroutine(FadeCanvasGroup(uiElement, 1, 0));
+        Invoke("destroyself", 1f);
     }
 	
+    void Update()
+    {
+        transform.Translate(0,movespeed,0);
+    }
+
+    public IEnumerator FadeCanvasGroup(CanvasGroup cg, float start, float end, float lerptime = 0.3f)
+    {
+        cg.alpha = 1;
+        float _timeStartedLerping = Time.time;
+        float timeSinceStarted = Time.time - _timeStartedLerping;
+        float percentageComplete = timeSinceStarted / lerptime;
+
+        while(true)
+        {
+            timeSinceStarted = Time.time - _timeStartedLerping;
+            percentageComplete = timeSinceStarted / lerptime;
+
+            float currentvalue = Mathf.Lerp(start, end, percentageComplete);
+
+            cg.alpha = currentvalue;
+            if (percentageComplete >= 1) break;
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+
+
+
 	void destroyself()
     {
-        iTween.Stop();
-        gameObject.SetActive(false); 
+         
+        Destroy(gameObject);
     }
 }
