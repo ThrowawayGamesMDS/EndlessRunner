e3a346b6418fcc2d7fc30655c8574252e14dbdd2
BlairCorban <blaircorban@hotmail.com>
2018-06-13 14:39:44 +1200

    what is love? baby DONUT hurt me

--- endlessrunnerfolder/Assets/Scripts/eatdonut.cs
+++ endlessrunnerfolder/Assets/Scripts/eatdonut.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class eatdonut : MonoBehaviour {
+    public GameObject particle;
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Player")
         {
-            
+            Instantiate(particle, transform.position, transform.rotation);
             Destroy(gameObject);
         }
     }
