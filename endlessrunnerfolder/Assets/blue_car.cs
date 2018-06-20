02612428bbce6d15f77009680193a0aa8e3ff2c9
MDS\nicholas.gre6280 <nik.green@xtra.co.nz>
2018-06-20 15:44:31 +1200

    more sound

---
+++ endlessrunnerfolder/Assets/blue_car.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blue_car : MonoBehaviour {
+	public AudioSource crash;
+	// Use this for initialization
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.GetComponent<PointManager> () && !ControllerHandler.m_bIsPaused) {
+			if (other.gameObject.GetComponent<PointManager> ().isWorthPoints == true) {
+				if (other.gameObject.GetComponent<PointManager> ().objName == "Player") {
+					crash.Play ();
+				}
+			}
+		}
+	}
+}
+
